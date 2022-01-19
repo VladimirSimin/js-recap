c2999fe9396d357ec670a9a3077e2ee2a2b9e8d8
Vladimir Simin <vladimir.simin@vivifyideas.com>
2022-01-19 10:44:31 +0100

    variable scope

---
+++ variable-scope.js
@@ -0,0 +1,17 @@
+// 1
+
+var testOut;
+function a (array) {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        var testVar;
+        let testLet;
+    }
+    console.log(testVar);
+    // console.log(testLet);
+    console.log(testOut);
+    var testIn;
+}
+// console.log(testIn);
+
+a([1, 2, 3]);
